ff4cfb82b0ae2963818f0812c8fa886cf6ca94d0
YOUR NAME <YOUR@EMAIL.com>
2021-11-29 22:52:16 +0700

    Ruby Hash - Initialization

---
+++ RubyHash-Initialization.rb
@@ -0,0 +1,21 @@
+# Initialize 3 variables here as explained in the problem statement
+
+empty_hash = Hash.new
+
+
+default_hash = Hash.new(1)
+
+# Way 2
+#default_hash =Hash.new
+#default_hash.default = 1
+
+
+hackerrank = {
+        "simmy" => 100,
+        "vivmbbs" =>200
+    }
+
+# Way 2
+# hackerrank = Hash.new
+# hackerrank["simmy"] = 100
+# hackerrank["vivmbbs"] = 200
